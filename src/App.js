905cdb4b577f961737937f23a7dbf2b6d7ba63ef
shawn <shawn@salin.co.kr>
2020-12-27 12:45:54 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
-import React from 'react'
-import NotificationBox from './component/NotificationBox';
-import Crontab from "reactjs-crontab"
-import 'reactjs-crontab/dist/index.css'
-
-
-const dashboardSetting = {
-  hidden: true
-  // if true, dashboard is hidden
-}
-
-const timeZone = "Asia/Seoul"
-// 'UTC', 'local, or 'YOUR PREFERRED TIMEZONE'
+import React from "react";
+import NotificationBox from "./component/NotificationBox";
+import Crontab from "reactjs-crontab";
+import "reactjs-crontab/dist/index.css";
 
 function App() {
-  const [ notification, setNotification ] = React.useState({
+  const [notification, setNotification] = React.useState({
     open: false,
     content: "",
-    btns: []
-  })
+    btns: [],
+  });
 
   const handleClose = () => {
     setNotification((prevState) => ({
       ...prevState,
-      open: false
-    }))
-  }
+      open: false,
+    }));
+  };
 
-  
   const renderFirstNoti = () => {
-  setNotification({
-    open: true,
-    header: "Message",
-    content: "Hello Tom! \n visit our Instagram",
-    btns: [{ id: "1", content: "Visit now", url: "https://instagram.com/" }]
-  })
+    setNotification({
+      open: true,
+      header: "Message",
+      content: "Hello Tom! \n visit our Instagram",
+      btns: [{ id: "1", content: "Visit now", url: "https://instagram.com/" }],
+    });
   };
 
   const renderSecondNoti = () => {
@@ -41,36 +31,43 @@ function App() {
       open: true,
       header: "Message",
       content: "Hello Tom! \n visit our Youtube chanel",
-      btns: [{ id: "2", content: "Visit now", url: "https://youtube.com" }]
-    })
-    };
+      btns: [{ id: "2", content: "Visit now", url: "https://youtube.com" }],
+    });
+  };
 
   const tasks = [
-      {
-        fn: renderFirstNoti,
-        // this is the function which is triggered based on the config
-        id: '1',
-        config: '*-*-*-*-*', // runs at every minutes
-        name: '', // optional
-        description: ''  // optional
-      }
-    ,
-      {
-        fn: renderSecondNoti,
-        // this is the function which is triggered based on the config
-        id: '2',
-        config: '*-17-19-12-*', // runs December 19th 10:10 AM
-        name: '',  // optional
-        description: ''  // optional
-      }
-    ]
-
+    {
+      fn: renderFirstNoti,
+      // this is the function which is triggered based on the config
+      id: "1",
+      config: "* * * * *", // runs at every minutes
+      name: "", // optional
+      description: "", // optional
+    },
+    {
+      fn: renderSecondNoti,
+      // this is the function which is triggered based on the config
+      id: "2",
+      config: "10 20 19 12 *", // runs December 19th 8:10 PM
+      name: "", // optional
+      description: "", // optional
+    },
+  ];
 
   return (
     <div>
-      <Crontab dashboard={dashboardSetting} timeZone={timeZone} tasks={tasks} />
+      <Crontab
+        tasks={tasks}
+        timeZone="Asia/Seoul"
+        // 'UTC', 'local, or 'YOUR PREFERRED TIMEZONE'
+        dashboard={{ hidden: false }}
+        // if true, dashboard is hidden
+      />
       {notification.open && (
-      <NotificationBox handleClose={handleClose} notification={notification} />
+        <NotificationBox
+          handleClose={handleClose}
+          notification={notification}
+        />
       )}
     </div>
   );
